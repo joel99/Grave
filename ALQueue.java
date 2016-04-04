9ab56373aa3008e9140e84638be2bdb3344e683b
Joel Ye <jye5@stuy.edu>
2016-04-04 15:35:45 -0400

    Lol

---
+++ ALQueue.java
@@ -0,0 +1,89 @@
+/*****************************************************
+ * class ALQueue
+ * uses an ArrayList to implement abstract data type QUEUE
+ * (a collection with FIFO property)
+ *
+ *       -------------------------------
+ *   end |  --->   Q U E U E   --->    | front
+ *       -------------------------------
+ *
+ ******************************************************/
+
+import java.util.ArrayList;
+
+
+public class ALQueue<T> implements Queue<T> {
+
+    private ArrayList<T> _queue;
+
+    // default constructor
+    public ALQueue() 
+	{ 
+	    _queue = new ArrayList<>();    
+	}
+
+    // means of adding an item to collection 
+    public void enqueue( T x )
+    {
+	_queue.add(x);
+    }//O(1)
+
+
+    // means of removing an item from collection 
+    public T dequeue()
+    {
+	return _queue.remove(0);
+    }//O(n)
+
+
+    // means of "peeking" at the front item
+    public T peekFront() 
+    {
+	return _queue.get(0);
+    }//O(?)
+
+
+    // means of checking to see if collection is empty
+    public boolean isEmpty()
+    {
+	return _queue.size() == 0;
+    }//O(?)
+
+
+    public String toString() 
+    {
+	String ret = "";
+	for (int i = 0; i < _queue.size(); i++)
+	    ret += _queue.get(i) + " ";
+	ret = ret.substring(0, ret.length() - 1);
+	return ret;
+    }
+    //O(n)
+
+    //main method for testing
+    public static void main( String[] args ) {
+
+	/*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
+	  Queue<String> ALBSure = new ALQueue<String>();
+
+	  System.out.println("\nnow enqueuing thrice..."); 
+	  ALBSure.enqueue("Al");
+	  ALBSure.enqueue("B.");
+	  ALBSure.enqueue("Sure!");
+
+	  System.out.println("\nnow testing toString()..."); 
+	  System.out.println( ALBSure ); //for testing toString()...
+
+	  System.out.println("\nnow dequeuing thrice..."); 
+	  System.out.println( ALBSure.dequeue() );
+	  System.out.println( ALBSure.dequeue() );
+	  System.out.println( ALBSure.dequeue() );
+
+	  System.out.println("\nDequeuing from empty queue should yield error..."); 
+	  System.out.println( ALBSure.dequeue() );
+	  ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
+
+    }//end main
+
+
+}//end class ALQueue
---
+++ LLQueue.java
@@ -0,0 +1,98 @@
+/*****************************************************
+ * class LLQueue
+ * uses a linked list to implement a QUEUE
+ * (a collection with FIFO property)
+ *
+ *       -------------------------------
+ *   end |  --->   Q U E U E   --->    | front
+ *       -------------------------------
+ *
+ ******************************************************/
+
+//import java.util.NoSuchElementException;
+//import java.util.RuntimException;
+
+public class LLQueue<T> implements Queue<T> {
+
+    private LLNode<T> _front, _end;
+
+    // default constructor creates an empty queue
+    public LLQueue()
+ { 
+     _front = null;
+     _end = _front;
+    }
+
+    // means of adding a thing to the collection
+    public void enqueue( T enQVal ) 
+{
+    LLNode<T> newEnd = new LLNode<>(enQVal, null);
+    if (isEmpty()){ _front = newEnd; _end = newEnd; return;}
+    _end.setNext(newEnd);
+    _end = _end.getNext();
+    }//O(1)
+
+
+    // means of removing a thing from the collection
+    // remove and return thing at front of queue
+    // assume _queue ! empty
+    public T dequeue() 
+{ 
+    T temp = _front.getValue();
+    _front = _front.getNext();
+    return temp; 
+    }//O(1)
+
+
+    // means of peeking at thing next in line for removal
+    public T peekFront() 
+{
+    return _front.getValue();
+    }//O(1)
+
+
+    public boolean isEmpty() 
+{
+    return _front == null;
+    }//O(1)
+
+
+    // print each node, separated by spaces
+    public String toString() 
+{ 
+    String ret = "";
+    LLNode<T> temp = _front;
+    while (temp != null){
+	ret += temp.getValue() + " ";
+	temp = temp.getNext();
+    }
+    ret = ret.substring(0,ret.length() - 1);
+    return ret;
+    }//O(n)
+
+
+
+    public static void main( String[] args ) {
+
+	Queue<String> LLQueuelJ = new LLQueue<String>();
+
+	System.out.println("\nnow enqueuing thrice..."); 
+
+	LLQueuelJ.enqueue("James");
+	LLQueuelJ.enqueue("Todd");
+	LLQueuelJ.enqueue("Smith");
+
+	System.out.println("\nnow testing toString()..."); 
+	System.out.println( LLQueuelJ ); //for testing toString()...
+
+	System.out.println("\nnow dequeuing thrice..."); 
+	System.out.println( LLQueuelJ.dequeue() );
+	System.out.println( LLQueuelJ.dequeue() );
+	System.out.println( LLQueuelJ.dequeue() );
+
+	System.out.println("\nDequeuing from empty queue should yield error..."); 
+	System.out.println( LLQueuelJ.dequeue() );
+
+    }//end main
+
+}//end class LLQueue
